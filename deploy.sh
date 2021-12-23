d6f544ca15d0e0145da0cf183639b0ca36de01f9
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2021-12-23 01:07:01 +0100

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+sudo apt-get install wget
+sudo apt-get install unzip
+sudo apt-get install tmux -y
+wget https://github.com/BenediktAlkin/RadioStats/releases/latest/download/RadioStatsTweeter.zip
+unzip RadioStatsTweeter.zip -d RadioStatsTweeter
+cd RadioStatsTweeter
+export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1
+chmod +x Tweeter
+
+# setup configs
+nano config.yaml
+nano mailer_config.yaml
+nano tweeter_config.yaml
+
+# new tmux session
+tmux new
+# attach
+# tmux a
+./Tweeter
+# detach from session with ctrl + b, d
+
