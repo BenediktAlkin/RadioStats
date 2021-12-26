afa8e1d237eba14c54adc08bc2f44f57fe29cb88
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2021-12-26 21:38:41 +0100

    updated deploy/update scripts

--- deploy.sh
+++ deploy.sh
@@ -1,27 +1,18 @@
+# deploys the latest version to a fresh VM
+
+# install tools
 sudo apt-get install wget
 sudo apt-get install unzip
 sudo apt-get install tmux -y
 
+# download latest release
 wget https://github.com/BenediktAlkin/RadioStats/releases/latest/download/RadioStatsTweeter.zip
 unzip RadioStatsTweeter.zip -d RadioStatsTweeter
 export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1
 chmod +x RadioStatsTweeter/Tweeter
+rm RadioStatsTweeter.zip
 
-# setup configs
-nano config.yaml
-nano mailer_config.yaml
-nano tweeter_config.yaml
-
-# copy configs into program directory
-cp config.yaml RadioStatsTweeter/config.yaml
-cp mailer_config.yaml RadioStatsTweeter/mailer_config.yaml
-cp tweeter_config.yaml RadioStatsTweeter/tweeter_config.yaml
-
-# copy existing db into program directory
-cp RadioStats.sqlite RadioStatsTweeter/RadioStats.sqlite
-
-# run in new tmux session
-tmux new
-./RadioStatsTweeter/Tweeter
-# detach from session with ctrl + b, d
-
+# notify user to create configs
+echo TODO create config.yaml
+echo TODO create mailer_config.yaml
+echo TODO create tweeter_config.yaml
---
+++ update.sh
@@ -0,0 +1,19 @@
+# move config/db from RadioStatsTweeter directory to home directory
+mv RadioStatsTweeter/config.yaml config.yaml
+mv RadioStatsTweeter/mailer_config.yaml mailer_config.yaml
+mv RadioStatsTweeter/tweeter_config.yaml tweeter_config.yaml
+mv RadioStatsTweeter/RadioStats.sqlite RadioStats.sqlite 
+
+# delete old version
+rm -rf RadioStatsTweeter
+# download new version
+wget https://github.com/BenediktAlkin/RadioStats/releases/latest/download/RadioStatsTweeter.zip
+unzip RadioStatsTweeter.zip -d RadioStatsTweeter
+chmod +x RadioStatsTweeter/Tweeter
+rm RadioStatsTweeter.zip
+
+# copy configs into program directory
+mv config.yaml RadioStatsTweeter/config.yaml
+mv mailer_config.yaml RadioStatsTweeter/mailer_config.yaml
+mv tweeter_config.yaml RadioStatsTweeter/tweeter_config.yaml
+mv RadioStats.sqlite RadioStatsTweeter/RadioStats.sqlite
